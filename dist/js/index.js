7197e36a4ecebb055958e628e21aceaf23e009ac
marcusMintchip <marcus.wang93@gmail.com>
2018-12-03 17:29:34 +0800

    nav-item change transition

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
+const navItem = document.querySelectorAll(".nav-item");
 const btnBox = document.querySelector(".btn-box");
 
 //set initial state of Menu
@@ -14,12 +15,14 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
+    navItem.forEach(item => item.classList.add("show"));
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
+    navItem.forEach(item => item.classList.remove("show"));
     showMenu = false;
   }
 }
